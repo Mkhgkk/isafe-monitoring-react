2d5aca555646d8d398ea85b286c792430a98ba97
Mkhgkk <emmachalz745@outlook.com>
2024-12-15 21:39:36 +0900

    chore: declare hostname dynamically

--- src/config/default.config.ts
+++ src/config/default.config.ts
@@ -1,5 +1,8 @@
 export default {
   // BACKEND_URL: "192.168.0.33:5000",
-  BACKEND_URL: "0.0.0.0:5000",
-  WEBRTC_STREAM_URL: "0.0.0.0:8889",
+  // BACKEND_URL: "0.0.0.0:5000",
+  // WEBRTC_STREAM_URL: "0.0.0.0:8889",
+  BACKEND_URL: `${window.location.hostname}:5000`,
+  WEBRTC_STREAM_URL: `${window.location.hostname}:8889`,
+  PROTOCOL: `${window.location.protocol}`,
 };
