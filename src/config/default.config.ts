b93135bc6defa005193f34e406c48aca89b29730
Mkhgkk <emmachalz745@outlook.com>
2024-12-19 14:38:16 +0900

    update: change connection urls

--- src/config/default.config.ts
+++ src/config/default.config.ts
@@ -1,8 +1,10 @@
+const hostname = window.location.hostname;
 export default {
   // BACKEND_URL: "192.168.0.33:5000",
-  // BACKEND_URL: "0.0.0.0:5000",
-  // WEBRTC_STREAM_URL: "0.0.0.0:8889",
-  BACKEND_URL: `192.168.0.33:5000`,
-  WEBRTC_STREAM_URL: `192.168.0.33:8889`,
+  BACKEND_URL: `${hostname}/api`,
+  WEBRTC_STREAM_URL: `${hostname}`,
+  SOCKETIO_URL: `${hostname}`,
+  // BACKEND_URL: `192.168.0.33:5000`,
+  // WEBRTC_STREAM_URL: `192.168.0.33:8889`,
   PROTOCOL: `${window.location.protocol}`,
 };
--- src/services/socketService.ts
+++ src/services/socketService.ts
@@ -1,11 +1,11 @@
 import { io } from "socket.io-client";
 import config from "../config/default.config";
 
-const SOCKET_SERVER_URL = `http://${config.BACKEND_URL}/video`;
+const SOCKET_SERVER_URL = `${config.SOCKETIO_URL}/video`;
 
 const socket = io(SOCKET_SERVER_URL, {
   autoConnect: true,
-  transports: ["websocket"],
+  transports: ["websocket", "polling"],
   upgrade: true,
 });
 
