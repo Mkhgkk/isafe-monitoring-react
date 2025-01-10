25ff79dfcc44267b8f1576be54dbc7fdef7689d1
Mkhgkk <emmachalz745@outlook.com>
2025-01-10 22:49:16 +0900

    feat: handle loading and error for webrtc player

--- src/components/webrtc-player.tsx
+++ src/components/webrtc-player.tsx
@@ -1,58 +1,67 @@
 import React, { useEffect, useState } from "react";
 import config from "../config/default.config";
+import { Icons } from "./icons";
+import { useTranslation } from "react-i18next";
 
 interface WebRTCPlayerProps {
   streamId: string;
 }
 
+const handledErrors = {
+  "peer connection closed": "connectionClosed",
+  "Error: bad status code 502": "serverNotAvailable",
+  "Error: stream not found": "streamNotFound",
+} as const;
+
 const WebRTCPlayer: React.FC<WebRTCPlayerProps> = ({ streamId }) => {
+  const { t } = useTranslation();
   const iframeSrc = `${config.PROTOCOL}//${config.WEBRTC_STREAM_URL}/live/${streamId}/`;
 
   const [videoLoading, setVideoLoading] = useState(true);
-  const [videoErrorMessage, setVideoErrorMessage] = useState<string>("");
+  const [videoErrorMessage, setVideoErrorMessage] = useState<string>();
   const [iframeLoaded, setIframeLoaded] = useState(false);
-  const [iframeErrorMessage, setIframeErrorMessage] = useState<string>("");
+  const [iframeErrorMessage, setIframeErrorMessage] = useState<string>();
 
   const handleMessage = (event: MessageEvent) => {
     if (event.data.target !== "mediamtx-webrtc-inpage") return;
-    // console.log(event.data);
 
-    if (event.data.type === "error") {
-      // show error
-      // event.data.message
-      // message type:
-      //  -- "peer connection closed" - cannot reach peer device either due to server being down or peer disconnected -- show retrying
-      //  -- "Error: bad status code 502" - failed to make connection with server
-      //  -- "Error: stream not found" - stream is is probably incorrect
+    switch (event.data.type) {
+      case "error": {
+        const msg = event.data.message;
 
-      setVideoErrorMessage(event.data.message);
-      console.log(event.data);
-    } else if (event.data.type === "streaming") {
-      // connection established
-      // hide error messages related to connection
-      // show loading
-      setVideoLoading(true);
-      setVideoErrorMessage("");
-      console.log(event.data);
-    } else if (event.data.type === "event") {
-      // canplay || suspended
-      // -- canplay hide loading
-      // -- suspended show loading
-      if (event.data.message === "canplay") setVideoLoading(false);
-      console.log(event.data);
-    } else {
-      // handle default case (probably error too)
-      //  -- unknown message
+        const errorKey = Object.keys(handledErrors).find((error) =>
+          msg.includes(error)
+        ) as keyof typeof handledErrors | undefined;
+
+        if (errorKey) {
+          const translationKey = handledErrors[errorKey];
+          setVideoErrorMessage(t(`stream.error.${translationKey}`));
+        } else {
+          setVideoErrorMessage(t(`stream.error.server`));
+        }
+
+        break;
+      }
+      case "streaming":
+        setVideoLoading(true);
+        setVideoErrorMessage(undefined);
+        break;
+      case "event":
+        if (event.data.message === "canplay") setVideoLoading(false);
+        break;
+      default:
+        setVideoErrorMessage(t("stream.error.server"));
+        break;
     }
   };
 
   const handleIframeError = () => {
     setIframeLoaded(false);
-    setIframeErrorMessage("Error: streaming server not reachable");
+    setIframeErrorMessage(t("stream.error.streamingServer"));
   };
 
   const handleIframeLoad = () => {
-    setIframeErrorMessage("");
+    setIframeErrorMessage(undefined);
     setIframeLoaded(true);
   };
 
@@ -66,6 +75,18 @@ const WebRTCPlayer: React.FC<WebRTCPlayerProps> = ({ streamId }) => {
 
   return (
     <div className="w-full h-full">
+      {(videoLoading || !iframeLoaded) && (
+        <div className="absolute inset-0 flex items-center justify-center w-full h-full bg-zinc-200 dark:bg-zinc-900">
+          <Icons.loading className="animate-spin" />
+        </div>
+      )}
+      {(videoErrorMessage || iframeErrorMessage) && (
+        <div className="absolute inset-0 flex items-center justify-center w-full h-full bg-zinc-200 dark:bg-zinc-900">
+          <p className="text-white">
+            {videoErrorMessage || iframeErrorMessage}
+          </p>
+        </div>
+      )}
       <iframe
         src={iframeSrc}
         title="WebRTC Stream"
