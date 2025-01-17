99935b32f830d457d554f104c7904422b5a885e8
Mkhgkk <emmachalz745@outlook.com>
2025-01-17 22:44:16 +0900

    update: namespace -> default

--- src/services/socketService.ts
+++ src/services/socketService.ts
@@ -1,7 +1,7 @@
 import { io } from "socket.io-client";
 import config from "../config/default.config";
 
-const SOCKET_SERVER_URL = `${config.SOCKETIO_URL}/video`;
+const SOCKET_SERVER_URL = `${config.SOCKETIO_URL}/default`;
 
 const socket = io(SOCKET_SERVER_URL, {
   autoConnect: true,
