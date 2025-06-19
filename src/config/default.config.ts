c34e8671c4bfeb3a0fb8eb597bfdfdc74f3c775f
Mkhgkk <emmachalz745@outlook.com>
2025-06-19 14:39:32 +0900

    update: hostname

--- src/config/default.config.ts
+++ src/config/default.config.ts
@@ -1,12 +1,8 @@
 const hostname = window.location.hostname;
 export default {
-  BACKEND_URL: "192.168.0.33:5000",
-  // BACKEND_URL: `${hostname}/api`,
-  // WEBRTC_STREAM_URL: `${hostname}`,
-  // SOCKETIO_URL: `${hostname}`,
+  // BACKEND_URL: "192.168.0.33:5000",
+  BACKEND_URL: `${hostname}/api`,
+  WEBRTC_STREAM_URL: `${hostname}`,
+  SOCKETIO_URL: `${hostname}`,
   PROTOCOL: `${window.location.protocol}`,
-  // BACKEND_URL: `isafe.re.kr//api`,
-  // WEBRTC_STREAM_URL: `isafe.re.kr/`,
-  // SOCKETIO_URL: `isafe.re.kr/`,
-  // PROTOCOL: `http://`,
 };
